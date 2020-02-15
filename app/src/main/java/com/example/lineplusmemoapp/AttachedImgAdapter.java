d1dccb5cd568b29a3be32cdc3eb0040878b2653f
Heeseung Jang <jang5934@gmail.com>
2020-02-16 08:01:07 +0900

    1. Image loading animation on recycler view is added

--- app/src/main/java/com/example/lineplusmemoapp/AttachedImgAdapter.java
+++ app/src/main/java/com/example/lineplusmemoapp/AttachedImgAdapter.java
@@ -1,38 +1,44 @@
 package com.example.lineplusmemoapp;
 
+import android.animation.ObjectAnimator;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AttachedImgAdapter extends RecyclerView.Adapter<AttachedImgViewHolder> {
-    private ArrayList<AttachedImg> attachedImgList = new ArrayList<AttachedImg>() ;
+    private List<AttachedImg> items = new ArrayList<>();
     @NonNull
     @Override
     public AttachedImgViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v;
 
-        // TODO 각 뷰의 레이아웃의 중류가 나뉘어져야 하는 경우 수정할 것
         if (viewType == 1) { // 현재 뷰가 사진을 포함한 뷰인 경우
-            v = LayoutInflater.from(
-                    parent.getContext()).inflate(R.layout.attached_image, parent, false);
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.attached_image, parent, false);
         }
         else {
-            v = LayoutInflater.from(
-                    parent.getContext()).inflate(R.layout.attached_image, parent, false);
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.empty_image, parent, false);
         }
-
         return new AttachedImgViewHolder(v);
     }
 
-    List<AttachedImg> items = new ArrayList<>();
     public void add(AttachedImg item) {
         items.add(item);
         // 정보를 받았을 때 뷰에 바로 반영해줌
@@ -45,11 +51,31 @@ public class AttachedImgAdapter extends RecyclerView.Adapter<AttachedImgViewHold
     public void onBindViewHolder(@NonNull AttachedImgViewHolder holder, int position) {
         AttachedImg item = items.get(position);
 
-        if(item.getImg_type() == 1) {
-            // String imgPath = item.getImageUri().getPath();
-            String imgPath = "https://bit.ly/2V1ipNj";
+        final AttachedImgViewHolder tHolder = holder;
 
-            // Glide.with(holder.mImg).load(imgPath).placeholder(R.drawable.placeholder).error(R.drawable.error_image).into(holder.mImg);
+        if(item.getImg_type() == 1) {
+
+            // String imgPath = item.getImageUri().getPath();
+            String imgPath = "https://raw.githubusercontent.com/bumptech/glide/master/static/glide_logo11.png";
+
+
+            Glide.with(holder.mImg)
+                 .load(imgPath)
+                 .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        tHolder.mProg.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, com.bumptech.glide.load.DataSource dataSource, boolean isFirstResource) {
+                        tHolder.mProg.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
+                    .error(R.mipmap.error_image)
+                    .into(holder.mImg);
         }
     }
 
@@ -61,7 +87,7 @@ public class AttachedImgAdapter extends RecyclerView.Adapter<AttachedImgViewHold
     @Override
     public int getItemViewType(int position) {
         // 사진이 담겨야하는 객체인 경우,
-        if(attachedImgList.size() != 0 && attachedImgList.get(position).getImg_type() == 1){
+        if(items.size() != 0 && items.get(position).getImg_type() == 1){
             return 1;
         }
         // 빈 사진인 경우(사진 추가 버튼 형태의 뷰)
--- app/src/main/java/com/example/lineplusmemoapp/AttachedImgViewHolder.java
+++ app/src/main/java/com/example/lineplusmemoapp/AttachedImgViewHolder.java
@@ -2,15 +2,18 @@ package com.example.lineplusmemoapp;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 public class AttachedImgViewHolder extends RecyclerView.ViewHolder {
 
     public ImageView mImg;
+    public ProgressBar mProg;
 
     public AttachedImgViewHolder(View itemView) {
         super(itemView);
         mImg = (ImageView) itemView.findViewById(R.id.attached_image);
+        mProg = (ProgressBar) itemView.findViewById(R.id.progress);
     }
 }
